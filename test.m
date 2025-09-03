81c48bd5b9070c56a0ca235924644776d9888e9f
yuantec <1345657010@qq.com>
2025-09-03 20:46:42 +0800

    湍流光传输

--- test.m
+++ test.m
@@ -31,3 +31,29 @@ trisurf(TR.ConnectivityList, pts(:,1), pts(:,2), I0, 'EdgeColor','none');
 view(2); shading interp; axis equal; colorbar;
 title('初始高斯光束辐照度分布');
 
+%% —— 湍流相关量 —— 
+Cn2   = 1e-16;         % 湍流结构常数 
+L0 = 100;              % 外尺度 
+l0 = 0.01;             % 内尺度 
+r0sw = (0.423*k^2*Cn2*3/8*Dz)^(-3/5);   % 平面波 Fried 参量 
+r0pw = (0.423*k^2*Cn2*Dz)^(-3/5);       % 球面波 Fried 参量 
+p = linspace(0,Dz,1e3); 
+rytov = 0.563*k^(7/6)*sum(Cn2*(1-p/Dz).^(5/6).*p.^(5/6)*(p(2)-p(1))); 
+
+%% —— 优化求解各相位屏 r0scrn —— 
+scr_count = 11;       % 相位屏数量 
+n = scr_count; 
+A = zeros(2,n); 
+alpha_vec = (0:n-1)/(n-1); 
+A(1,:) = alpha_vec.^(5/3); 
+A(2,:) = (1-alpha_vec).^(5/6).*alpha_vec.^(5/6); 
+b = [r0sw^(-5/3); rytov/1.33*(k/Dz)^(5/6)]; 
+x0 = (n/3*r0sw*ones(n,1)).^(-5/3); 
+x1 = zeros(n,1); 
+rmax = 0.1; 
+x2 = rmax/1.33*(k/Dz)^(5/6)./A(2,:); 
+x2(A(2,:)==0) = 50^(-5/3); 
+opts = optimoptions('fmincon','Display','none'); 
+[X,~,~,~] = fmincon(@(X) sum((A*X - b).^2), x0, [],[],[],[], x1, x2, [], opts); 
+r0scrn = X.^(-3/5); 
+r0scrn(isinf(r0scrn)) = 1e6; 
